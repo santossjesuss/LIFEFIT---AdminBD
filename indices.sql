aa522f2779c34ea8c3e180d9cfaf3272a57694ac
AlvaroDolz <a.dolzdelcastellar1@udc.es>
2024-04-19 11:43:43 +0200

    Cambios

--- indices.sql
+++ indices.sql
@@ -1,6 +1,6 @@
 -- Entendemos que los atributo por los que más vamos a buscar usuarios son: correo electrónico, teléfono y, nombre y apellidos.
 -- Creamos indices para estas columnas de Usuario:
-CREATE UNIQUE INDEX idx_usuario_correo ON usuario(correo_e) TABLESPACE TS_INDICES;
+CREATE UNIQUE INDEX idx_usuario_correo ON usuario(correoe) TABLESPACE TS_INDICES;
 CREATE UNIQUE INDEX idx_usuario_telefono ON usuario(telefono) TABLESPACE TS_INDICES;
 -- creo que es case insensitive
 CREATE INDEX idx_usuario_nombre_apellidos ON usuario(UPPER(nombre), UPPER(apellidos)) TABLESPACE TS_INDICES;
