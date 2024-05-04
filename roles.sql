688bc9026540fcd4fd14a30863ea2194ac038de5
AlvaroDolz <a.dolzdelcastellar1@udc.es>
2024-05-04 16:58:18 +0200

    Roles

--- roles.sql
+++ roles.sql
@@ -1,24 +1,28 @@
 -- Creamos roles:
-CREATE ROLE Administrador;
-CREATE ROLE Gerente;
-CREATE ROLE Entrenador_deporte;
-CREATE ROLE Entrenador_nutricion;
-CREATE ROLE Cliente;
+CREATE ROLE administrador;
+CREATE ROLE gerente;
+CREATE ROLE entrenador_deporte;
+CREATE ROLE entrenador_nutricion;
+CREATE ROLE cliente;
 
--- Asignación de permisos:
-GRANT ALL PRIVILEGES ON TS_LIFEFIT TO Administrador;
+-- 1. RF1. Gestión de los Ejercicios.
 
-GRANT ALL PRIVILEGES ON TS_LIFEFIT.centro TO gerente;
-GRANT ALL PRIVILEGES ON TS_LIFEFIT.entrenador TO gerente;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TS_LIFEFIT.cliente TO gerente;
-
-GRANT ALL PRIVILEGES ON TS_LIFEFIT.ejercicio TO Entrenador_deporte;
-GRANT ALL PRIVILEGES ON TS_LIFEFIT.rutina TO Entrenador_deporte;
-GRANT ALL PRIVILEGES ON TS_LIFEFIT.conforman TO Entrenador_deporte;
-GRANT ALL PRIVILEGES ON TS_LIFEFIT.sesion TO Entrenador_deporte;
-GRANT ALL PRIVILEGES ON TS_LIFEFIT.plan TO Entrenador_deporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ejercicio TO entrenador_deporte;
+ALTER TABLE ejercicio ADD PUBLICO CHAR(1) DEFAULT 'S';
+CREATE VIEW VEJERCICIO AS SELECT * FROM ejercicio WHERE PUBLICO = 'S';
+INSERT INTO ejercicio (id, nombre, descripcion, video) SELECT ROWNUM, nombre, descripcion, video FROM ejercicios_ext;
 
 
+-- 2. RF3. Gestión de la rutina.
+GRANT SELECT, INSERT, UPDATE, DELETE ON rutina TO entrenador_deporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON conforman TO entrenador_deporte;
 
--- Asignación de role a usuario:
-GRANT Administrador TO LIFEFIT;
+-- 3. RF2. Asignación de clientes a entrenadores.
+GRANT SELECT, INSERT, UPDATE, DELETE ON entrena TO gerente;
+
+-- 4. RF4. Asignación de plan y sesiones de entrenamiento.
+-- - Los entrenadores podrán asignar planes, rutinas y sesiones de ejercicios a los clientes.
+GRANT SELECT, INSERT, UPDATE, DELETE ON plan TO entrenador_deporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON sesion TO entrenador_deporte;
+
+
