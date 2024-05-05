b06c1330014c1289dc94ba040b4c3784a50128ef
AlvaroDolz <a.dolzdelcastellar1@udc.es>
2024-05-05 13:51:48 +0200

    Arreglos finales roles

--- roles1.sql
+++ roles1.sql
@@ -44,8 +44,8 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON entrena TO gerente;
 -- 4. RF4. Asignación de plan y sesiones de entrenamiento
 -- - Los entrenadores podrán asignar planes, rutinas y sesiones de ejercicios a los clientes
 CREATE VIEW VPLAN_ENTRENADOR AS SELECT * FROM plan WHERE entrena_entrenador_id = (SELECT id FROM usuario WHERE usuariooracle = USER);
-GRANT SELECT, INSERT, UPDATE, DELETE ON VPLAN TO entrenador_deporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON VPLAN_ENTRENADOR TO entrenador_deporte;
 
 CREATE VIEW VSESION_ENTRENADOR AS SELECT inicio, fin, presencial, descripcion, plan_inicio, plan_rutina_id, plan_entrena_entrenador_id, plan_entrena_cliente_id 
                        FROM sesion WHERE plan_entrena_entrenador_id = (SELECT id FROM USUARIO WHERE usuariooracle = USER);
-GRANT SELECT, INSERT, UPDATE, DELETE ON VSESION TO entrenador_deporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON VSESION_ENTRENADOR TO entrenador_deporte;
