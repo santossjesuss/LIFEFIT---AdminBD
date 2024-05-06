96bb1c0f9abfe29bce67d9ce11eff1336e0f0577
AlvaroDolz <a.dolzdelcastellar1@udc.es>
2024-05-06 20:07:56 +0200

    COmmit

--- indices.sql
+++
@@ -1,8 +0,0 @@
--- Entendemos que los atributo por los que más vamos a buscar usuarios son: correo electrónico, teléfono y, nombre y apellidos.
--- Creamos indices para estas columnas de Usuario:
-CREATE UNIQUE INDEX idx_usuario_correo ON usuario(correoe) TABLESPACE TS_INDICES;
-CREATE UNIQUE INDEX idx_usuario_telefono ON usuario(telefono) TABLESPACE TS_INDICES;
--- creo que es case insensitive
-CREATE INDEX idx_usuario_nombre_apellidos ON usuario(UPPER(nombre), UPPER(apellidos)) TABLESPACE TS_INDICES;
--- Creamos indice de tipo BITMAP en centro_id
-CREATE BITMAP INDEX idx_cliente_centro_id ON cliente(centro_id) TABLESPACE TS_INDICES;
