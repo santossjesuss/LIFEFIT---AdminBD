15f24e19d72b81882344c40fe4acdf57db4766df
Yisus <bsantossjesus@gmail.com>
2024-05-29 13:18:13 +0200

    extras

--- indices.sql
+++ indices.sql
@@ -5,6 +5,6 @@
 -- En Oracle, cuando creas una tabla con columnas que tienen la restricción UNIQUE, 
 -- el sistema automáticamente crea índices únicos en esas columnas para garantizar la unicidad, como es el caso de correoe y telefono.
 
-CREATE INDEX idx_usuario_nombre_apellidos ON usuario(UPPER(nombre), UPPER(apellidos)) TABLESPACE TS_INDICES;
+CREATE INDEX "idx_usuario_nombre_apellidos" ON usuario(UPPER(nombre), UPPER(apellidos)) TABLESPACE TS_INDICES;
 -- Creamos indice de tipo BITMAP en centro_id
-CREATE BITMAP INDEX idx_cliente_centro_id ON cliente(centro_id) TABLESPACE TS_INDICES;
+CREATE BITMAP INDEX "idx_cliente_centro_id" ON cliente(centro_id) TABLESPACE TS_INDICES;
---
+++ vistas_adicionales.sql
@@ -0,0 +1,10 @@
+CREATE VIEW full_datos_entrenador AS
+SELECT 
+    e.id AS entrenador_id,
+    e.disponibilidad,
+    e.centro_id,
+    t.especialidad
+FROM 
+    entrenador e
+JOIN 
+    entrena t ON e.id = t.entrenador_id;
